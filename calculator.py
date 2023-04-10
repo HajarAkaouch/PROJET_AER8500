ebae7f4716a3e1bc1d1d660d8ea442754f8012bd
DhiaEddineAssli <dhia.assli12@hotmail.com>
2023-04-10 15:02:41 -0400

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -14,24 +14,40 @@ def user_input(altitude_desiree, taux_monte, angle_attaque):
     temps_ecoule = 0
 
     #Imputs de l'utilisateur (À mettre dans le while, mais pour l'instant à l'extérieur juste pour les tests)
-    altitude_desiree = float(input("Entrez l'altitude désirée : "))
-    taux_monte = float(input("Entrez le taux de montée : "))
-    angle_attaque = float(input("Entrez l'angle d'attaque : "))
+    altitude_desiree = float(input("Entrez l'altitude désirée (pieds) : "))
+    taux_monte = float(input("Entrez le taux de montée (m/min) : "))
+    angle_attaque = float(input("Entrez l'angle d'attaque (degrés) : "))
 
     while True:
+        #******************************************* AFFICHAGE **********************************************
+
         # Affichage des valeurs actuelles de l'altitude, de la vitesse et de la puissance du moteur
-        print(f"Altitude actuelle : {altitude_actuelle}")
-        print(f"Vitesse actuelle : {vitesse_actuelle}")
-        print(f"Puissance moteur : {puissance_moteur}")
+        print(f"Altitude actuelle (pieds) : {altitude_actuelle}")
+        print(f"Vitesse actuelle (pieds/sec) : {vitesse_actuelle}")
+        print(f"Puissance moteur (W) : {puissance_moteur}")
         print(f"État système : {etat_systeme}")
-        print(f"Temps écoulé : {temps_ecoule}")
-        
+        print(f"Temps écoulé (s) : {temps_ecoule}")
+
+
+        #*************************************** RÉACTIVITÉ & CLOCK **********************************************
+
         # Pause de 1 seconde avant d'actualiser les valeurs (va changer la réactivité de notre système)
         time.sleep(1)
 
         #Clock
         temps_ecoule += 1
+
+
+        #************************************** FONCTIONS DE CONVERSION ******************************************
         
+        # Conversion du taux de montée entré par l'utilisateur (m/min -> pieds/s)
+        taux_monte_convertit = (taux_monte*3.28084)/60 # 1 m = 3.28084 pieds
+        
+        # Conversion de la vitesse actuelle (pieds/s -> km/h)
+        # À faire
+
+        #********************************************** GROUND ******************************************
+
         if etat_systeme == "GROUND":
             # Si l'utilisateur entre une valeur d'altitude désirée de 0
             while altitude_desiree == 0:
@@ -57,20 +73,24 @@ def user_input(altitude_desiree, taux_monte, angle_attaque):
             else:
                 etat_systeme = "CHANGEMENT_ALT"
 
+
+        #************************************** CHANG. ALTITUDE ******************************************
+
         elif etat_systeme == "CHANGEMENT_ALT":
             # Calcul de la vitesse en fonction de la puissance moteur (V = TM/sin(angle))
-            vitesse_actuelle = 100 / math.sin(math.radians(angle_attaque)) 
+            vitesse_actuelle = 5.468 / math.sin(math.radians(angle_attaque)) # 100m/min -> 5.48pieds/s
 
             # Calcul de la nouvelle altitude
-            altitude_actuelle += taux_monte * temps_ecoule
+            altitude_actuelle += taux_monte_convertit * temps_ecoule
 
-            #Calcul la puissance?
-            #blabla 
+            # Calcul la puissance?
+            # À faire 
 
             # Sortie de l'état si l'altitude désirée est atteinte
             if altitude_actuelle >= altitude_desiree or altitude_actuelle >= 40000:
                 etat_systeme = "VOL_CROISIÈRE"
             
+        #*************************************** VOL CROISIÈRE ******************************************
 
         elif etat_systeme == "VOL_CROISIÈRE":  
             # taux mis à zéro une fois l'altitude atteinte
@@ -82,8 +102,8 @@ def user_input(altitude_desiree, taux_monte, angle_attaque):
             elif altitude_actuelle < altitude_desiree:
                 vitesse_actuelle += 10
 
-            #Calcul la puissance? 
-            #blabla
+            # Calcul la puissance? 
+            # À faire
 
 
 
